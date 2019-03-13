04ce71c0434cba3879bb14994645087f0a855262
Ian Homer <ian.homer@gmail.com>
2019-03-13 22:40:52 +0000

    Report on meta data in file

--- audio_meta.py
+++ audio_meta.py
@@ -1,16 +1,34 @@
 import eyed3
-from os import walk
+import os
+from pathlib import Path
 
+audioExtensions = ["mp3", "wav", "ogg"]
 
 files = []
-for (dirpath, dirnames, filenames) in walk("."):
-    files.extend(list(filter(lambda f : f.endswith(".mp3"), filenames)))
+for (dirpath, dirnames, filenames) in os.walk("."):
+    files.extend(list(filter(lambda f : Path(f).suffix[1:] in audioExtensions, filenames)))
     break
 
-for file in files:
-    print(file)
+def mp3handler(file):
     audio = eyed3.load(file)
-    if (audio.tag):
-        print("Artist : %s" % audio.tag.artist)
-    else:
-        print("No audio tag")
+    return {
+        "album"     : audio.tag.album,
+        "artist"    : audio.tag.artist,
+        "title"     : audio.tag.title
+    }
+
+def defaultHandler(file):
+    return {
+        "album"     : "n/a",
+        "artist"    : "n/a",
+        "title"     : "n/a"
+    }
+
+print(f"{'ext':4s} : {'name':30s} : {'artist':10s} : {'title':15s} : album")
+print(80*"-")
+for file in files:
+    path = Path(file)
+    ext = path.suffix[1:]
+    stem = path.stem
+    meta = {"mp3" : mp3handler}.get(ext, defaultHandler)(file)
+    print(f"{ext:4s} : {stem:30s} : {meta['artist']:10s} : {meta['title']:15s} : {meta['album']}")
