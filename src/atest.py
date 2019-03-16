b6555dd74c22b4a9298559ee7e40c369e44f26f4
Ian Homer <ian.homer@gmail.com>
2019-03-16 16:02:13 +0000

    atest now in python

--- src/atest.py
+++ src/atest.py
@@ -3,6 +3,12 @@ from pathlib import Path
 import os
 import ffmpeg
 
+replace = True
+quiet = True
+ogg = False
+mp3 = True
+m4a = False
+
 class Destination:
     def __repr__(self):
         return f"{self.quality} {self.ext}"
@@ -13,6 +19,13 @@ class Destination:
             return f"q{self.quality}"
         else:
             return f"{self.quality}k"
+
+    @property
+    def ffmpegArgs(self):
+        if self.ext == "ogg":
+            return {"qscale:a":self.quality}
+        else:
+            return {"b:a":f"{self.quality}k"}
 
     def __init__(self, ext, quality):
         self.ext = ext
@@ -27,10 +40,27 @@ class Channels:
         self.channels = channels
 
 
-destinations = [
-    Destination('ogg', 1),
-    Destination('ogg', 5)
-]
+destinations = []
+if ogg:
+    destinations.extend([
+        Destination('ogg', 1),
+        Destination('ogg', 5),
+        Destination('ogg', 10)
+    ])
+if mp3:
+    destinations.extend([
+        Destination('mp3', 32),
+        Destination('mp3', 64),
+        Destination('mp3', 128),
+        Destination('mp3', 256)
+    ])
+if m4a:
+    destinations.extend([
+        Destination('m4a', 32),
+        Destination('m4a', 64),
+        Destination('m4a', 128),
+        Destination('m4a', 256)
+    ])
 
 channelsList = [Channels(1), Channels(2)]
 
@@ -48,8 +78,14 @@ def run():
 
     for destination in destinations:
         for channels in channelsList:
-            print(f"Converting {destination} : {channels}")
+            print(f"Converting {destination} : {channels} : {destination.ffmpegArgs}")
             outFile=f"{title}/{title} - {channels} {destination.variationName}.{destination.ext}"
-            stream = ffmpeg.input(audioIn)
-            stream = ffmpeg.output(stream, outFile)
-            ffmpeg.run(stream)
+            (
+                ffmpeg
+                    .input(audioIn)
+                    .output(outFile, **destination.ffmpegArgs)
+                    .run(**{
+                        'quiet':quiet,
+                        'overwrite_output':replace
+                    })
+            )
