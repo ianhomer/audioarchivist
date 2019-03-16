ba8ce422a19f4fa4dca61a2584cfa061b49bcb08
Ian Homer <ian.homer@gmail.com>
2019-03-16 12:07:47 +0000

    Make directory

--- src/atest.py
+++ src/atest.py
@@ -1,5 +1,12 @@
 import sys
+from pathlib import Path
+import os
 
 def run():
     audioIn = sys.argv[1]
-    print(f"Converting audio files for testing : {audioIn}")
+    path = Path(audioIn)
+    title = path.stem.split('-')[0].strip()
+    print(f"Converting audio files for testing : {audioIn} : {title}")
+    if not os.path.exists(title):
+        print(f"Making directory {title}")
+        os.makedirs(title)
