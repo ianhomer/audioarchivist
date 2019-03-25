a8457797101a0843e2b54a0f54a1908b1a765d77
Ian Homer <ian.homer@gmail.com>
2019-03-25 15:02:21 +0000

    Improve song test ; add requirements file

--- audioarchivist/tests/test_song.py
+++ audioarchivist/tests/test_song.py
@@ -12,9 +12,26 @@ class TestSong(TestCase):
         self.assertEqual(song.album, "My Album")
         self.assertEqual(song.artist, "Purpley")
         self.assertEqual(song.title, "Sound")
+        self.assertTrue(song.stemAligned)
 
     def test_song_from_nometa(self):
         song = Song(testDirectory + "/storage/nometa/samples/Test001 - samples - Purpley.mp3")
         self.assertEqual(song.album, "samples")
         self.assertEqual(song.artist, "Purpley")
         self.assertEqual(song.title, "Test001")
+        self.assertTrue(song.stemAligned)
+
+    def test_song_title_misaligned(self):
+        # when load with out naming flag
+        song = Song(testDirectory + "/storage/nometa/samples/Test002 (Title Misaligned) - samples - Purpley.mp3")
+        # then title comes from file meta data
+        self.assertEqual(song.title, "Test001")
+        self.assertEqual(song.album, "samples")
+        self.assertEqual(song.artist, "Purpley")
+        # and stem is misaligned
+        self.assertFalse(song.stemAligned)
+        self.assertEqual(song.alt['title'], "Test002 (Title Misaligned)")
+        # when load with naming flag
+        song = Song(testDirectory + "/storage/nometa/samples/Test002 (Title Misaligned) - samples - Purpley.mp3", True)
+        # then title comes from file name
+        self.assertEqual(song.title, "Test002 (Title Misaligned)")
