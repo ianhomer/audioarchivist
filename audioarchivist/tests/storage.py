76e4eb6882c850d751c60017cef16808c5b8a449
Ian Homer <ian.homer@gmail.com>
2019-03-25 16:48:34 +0000

    Provide storage class for easy of managing test files

---
+++ audioarchivist/tests/storage.py
@@ -0,0 +1,32 @@
+import os
+
+from pathlib import Path
+from shutil import copyfile
+
+testDirectory = os.path.join(os.path.dirname(__file__))
+storageDirectory = os.path.join(testDirectory, "storage")
+tmpDirectory = os.path.join(testDirectory, "tmp")
+
+protoypes = {
+    "mp3": storageDirectory + "/prototypes/Test000 - prototypes - Purpley.mp3"
+}
+
+class Storage:
+    def __init__(self):
+        self.tmpDirectory = tmpDirectory
+        self.storageDirectory = storageDirectory
+
+    def getFilename(self, relativeFilename):
+        return os.path.join(storageDirectory, relativeFilename)
+
+    def getTmpFilename(self, relativeFilename):
+        return os.path.join(tmpDirectory, relativeFilename)
+
+    def createTmp(self, protoype, name):
+        newFilename = os.path.join(tmpDirectory, name)
+        directory = Path(newFilename).parent
+        if not os.path.exists(directory):
+            print(f"Making tests tmp directory {directory}")
+            os.makedirs(directory)
+        copyfile(protoypes[protoype], newFilename)
+        return newFilename
--- audioarchivist/tests/test_song.py
+++ audioarchivist/tests/test_song.py
@@ -1,37 +1,38 @@
-import os
-
 from unittest import TestCase
 
 from audioarchivist.song import Song
+from audioarchivist.tests.storage import Storage
 
-testDirectory = os.path.join(os.path.dirname(__file__))
+storage = Storage()
 
 class TestSong(TestCase):
     def test_song_from_collections(self):
-        song = Song(testDirectory + "/storage/collections/samples/My Album/Sound - My Album - Purpley.wav")
+        song = Song(storage.getFilename("collections/samples/My Album/Sound - My Album - Purpley.wav"))
         self.assertEqual(song.album, "My Album")
         self.assertEqual(song.artist, "Purpley")
         self.assertEqual(song.title, "Sound")
         self.assertTrue(song.stemAligned)
 
-    def test_song_from_nometa(self):
-        song = Song(testDirectory + "/storage/nometa/samples/Test001 - samples - Purpley.mp3")
-        self.assertEqual(song.album, "samples")
+    def test_prototypes_song(self):
+        song = Song(storage.getFilename("prototypes/Test000 - prototypes - Purpley.mp3"))
+        self.assertEqual(song.album, "prototypes")
         self.assertEqual(song.artist, "Purpley")
-        self.assertEqual(song.title, "Test001")
+        self.assertEqual(song.title, "Test000")
         self.assertTrue(song.stemAligned)
 
     def test_song_title_misaligned(self):
+        filename = storage.createTmp("mp3", "nometa/samples/Test002 (Title Misaligned) - prototypes - Purpley.mp3")
+
         # when load with out naming flag
-        song = Song(testDirectory + "/storage/nometa/samples/Test002 (Title Misaligned) - samples - Purpley.mp3")
+        song = Song(filename)
         # then title comes from file meta data
-        self.assertEqual(song.title, "Test001")
-        self.assertEqual(song.album, "samples")
+        self.assertEqual(song.title, "Test000")
+        self.assertEqual(song.album, "prototypes")
         self.assertEqual(song.artist, "Purpley")
         # and stem is misaligned
         self.assertFalse(song.stemAligned)
         self.assertEqual(song.alt['title'], "Test002 (Title Misaligned)")
         # when load with naming flag
-        song = Song(testDirectory + "/storage/nometa/samples/Test002 (Title Misaligned) - samples - Purpley.mp3", True)
+        song = Song(filename, True)
         # then title comes from file name
         self.assertEqual(song.title, "Test002 (Title Misaligned)")
