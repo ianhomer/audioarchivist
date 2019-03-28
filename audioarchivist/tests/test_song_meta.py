e646d019402b427260b91534647177655ca2c30c
Ian Homer <ian.homer@gmail.com>
2019-03-28 23:02:36 +0000

    Improve sequence of tests

--- audioarchivist/tests/test_song_meta.py
+++ audioarchivist/tests/test_song_meta.py
@@ -10,10 +10,13 @@ class TestSongMeta(TestCase):
     def test_song_meta(self):
         storage.tmp("meta-artist", "meta/test/meta/.ameta.yaml")
         filename = storage.tmp("mp3", "meta/test/meta/Test000.mp3")
-        storage.tmp("mp3", "meta/test2/meta/Test000.mp3")
         song = Song(filename)
         self.assertEqual("Purpley", song.artist)
-        self.assertEqual(1, len(song.alternatives))
-        self.assertEqual("Test000", song.alternatives[0].stem)
+        # When load by name
         song = Song(filename, True)
         self.assertEqual("meta-artist", song.artist)
+
+        # Given alternative is available
+        storage.tmp("mp3", "meta/test2/meta/Test000.mp3")
+        self.assertEqual(1, len(song.alternatives))
+        self.assertEqual("Test000", song.alternatives[0].stem)
